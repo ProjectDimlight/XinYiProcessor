17ece73982517810f48fc5b73c3768751c030cfb
PAN-Ziyue <alphavake2018@gmail.com>
2021-04-17 04:31:40 +0800

    add WB.scala

---
+++ src/main/scala/xinyi_s5i4_bc/stages/WB.scala
@@ -0,0 +1,23 @@
+package xinyi_s5i4_bc.stages
+
+
+import chisel3._
+import chisel3.util._
+import config.config._
+import xinyi_s5i4_bc.parts.ControlConst._
+
+
+class WBIO extends Bundle {
+    val write_target = Input(UInt(write_target_w.W))
+    val data = Input(UInt(XLEN.W))
+    val rd = Input(UInt(XLEN.W))
+    val id = Input(UInt(ISSUE_NUM_W.W))
+    val ready = Input(Bool())
+    //>>>>>
+    val pc = Input(UInt(XLEN.W))
+}
+
+
+class WB {
+
+}
