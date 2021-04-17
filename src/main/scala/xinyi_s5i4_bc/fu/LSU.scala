1332111f9ea6cfa3eece410125109bb089d09a87
SOL <3160105063@zju.edu.cn>
2021-04-17 19:54:36 +0800

    Added LSU

--- src/main/scala/xinyi_s5i4_bc/fu/LSU.scala
+++ src/main/scala/xinyi_s5i4_bc/fu/LSU.scala
@@ -12,12 +12,43 @@ import xinyi_s5i4_bc.stages._
  */
 class LSU(addr_bits: Int, lsu_ctrl_bits: Int) extends Module {
   val io = IO(new Bundle {
-    val input   = new PathInterface 
-    val output  = Flipped(new WBInterface)
+    val in   = new FUIn 
+    val out  = new FUOut
 
-    val exception_id  = Input(UInt(ISSUE_NUM.W))
-    val exception     = Output(UInt(ISSUE_NUM.W))
+    // To DCache
+    val cache           = Flipped(new RAMInterface(LGC_ADDR_W, L1_W))
+    val stall_req       = Input(Bool())
+    
+    val exception_order = Input(UInt(ISSUE_NUM.W))
   })
 
+  val addr = Wire(UInt(LGC_ADDR_W.W))
+  addr := in.a + in.imm
+
+  val normal = exception_order > in.order
+
+  io.cache.wt   := normal & (in.write_target === DMem)
+  io.cache.rd   := normal & (in.rd =/= 0.U)  
+  io.cache.addr := addr
+  io.cache.din  := rt
+  
+  out.hi        := addr
+  out.data      := io.cache.dout
+  out.ready     := !cache.stall_req
+
+  out.write_target := in.write_target
+  out.rd           := in.rd
+  out.order        := in.order
+  out.pc           := in.pc
+  out.exception    := MuxLookup(
+    in.fu_ctrl,
+    false.B,
+    Array(
+      MemHalf  -> addr(0) === 0.U(1.W),
+      MemHalfH -> addr(0) === 0.U(1.W),
+      MemWord  -> addr(1, 0) === 0.U(2.W)
+    )
+  )
+  
 }
 
