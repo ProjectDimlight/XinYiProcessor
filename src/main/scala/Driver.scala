1d3e57b834263d7d07a384094fbc1dbafee0d88f
PAN-Ziyue <alphavake2018@gmail.com>
2021-04-22 13:54:41 +0800

    add global driver for generating verilog code

---
+++ src/main/scala/Driver.scala
@@ -0,0 +1,16 @@
+import chisel3.stage.{ChiselGeneratorAnnotation, ChiselStage}
+import xinyi_s5i4_bc.stages.WBStage
+
+//>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>
+//      this is a demo for SOL dalao
+//
+// "-td" is short for "--target-directory",
+// and the sequenced target dir.
+//<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<
+object Driver extends App {
+  (new ChiselStage).execute(
+    Array("-X", "verilog", "-td", "verilog"),
+    Seq(ChiselGeneratorAnnotation(() => new WBStage))
+  )
+}
+
