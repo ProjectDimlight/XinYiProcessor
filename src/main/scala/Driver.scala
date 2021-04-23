a14cc455b4681e5ba53195fb020a278df4881521
PAN-Ziyue <alphavake2018@gmail.com>
2021-04-23 09:04:03 +0800

    fix elaboration of CP0, add prettified prompt

--- src/main/scala/Driver.scala
+++ src/main/scala/Driver.scala
@@ -1,4 +1,5 @@
 import chisel3.stage.{ChiselGeneratorAnnotation, ChiselStage}
+import xinyi_s5i4_bc.fu.CP0
 import xinyi_s5i4_bc.stages.WBStage
 
 //>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>
@@ -8,9 +9,35 @@ import xinyi_s5i4_bc.stages.WBStage
 // and the sequenced target dir.
 //<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<
 object Driver extends App {
+
+
+  //>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>
+  //      some prettified prompt
+  //
+  //    basically it is out of
+  //  ziyue's personal taste.
+  //<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<
+  val XINYI = Console.BOLD + Console.YELLOW + "\n[XinYiProcessor] " + Console.RESET
+
+  def prompt(module_name: String) = {
+    println(XINYI + "generating verilog code for " + Console.CYAN + module_name + Console.RESET)
+  }
+
+  //>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>
+  //    start generating verilog code
+  //<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<
+
+  prompt("WBStage")
   (new ChiselStage).execute(
     Array("-X", "verilog", "-td", "verilog"),
     Seq(ChiselGeneratorAnnotation(() => new WBStage))
+  )
+
+
+  prompt("CP0")
+  (new ChiselStage).execute(
+    Array("-X", "verilog", "-td", "verilog"),
+    Seq(ChiselGeneratorAnnotation(() => new CP0))
   )
 }
 
